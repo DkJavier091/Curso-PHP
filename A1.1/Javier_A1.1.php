17847c0664d8c4daefc3300eef87c1ab7801b92a
Javier Agundo García <javier.agundo@icloud.com>
2024-11-07 10:28:29 +0100

    Se ha creado el curso

---
+++ A1.1/Javier_A1.1.php
@@ -0,0 +1,21 @@
+<?php
+$name ="Javier";
+$price = 2;
+?>
+
+<!doctype html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="../Css/styles.css"
+    <title>Javier_A1.1</title>
+</head>
+<body>
+    <h1>The Candy Store</h1>
+    <h2>Welcome <?php echo $name;?> </h2>
+    <p>The cost of your candy is
+        $<?php echo $price;?> per pack.
+    </p>
+</body>
+</html>
